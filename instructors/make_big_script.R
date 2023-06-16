2f0267776e1dc8a371e0c729177daab9f3042b2b
ajrominger <ajrominger@gmail.com>
2023-06-16 14:56:35 -0600

    trying to get rid of evaluation of problematic packages so the site builds

--- instructors/make_big_script.R
+++ instructors/make_big_script.R
@@ -1,29 +1,29 @@
-f <- c("episodes/introduction.Rmd",
-       "episodes/care.Rmd",
-       "episodes/abundance-data.Rmd",
-       "episodes/traits-data.Rmd",
-       "episodes/phylo-data.Rmd",
-       "episodes/popgen-data.Rmd",
-       "episodes/connecting-data-types.Rmd",
-       "episodes/finding_data.Rmd",
-       "episodes/care-data-repos.Rmd")
-
-for(i in f) {
-    knitr::purl(i, documentation = 0)
-}
-
-fr <- gsub('\\.Rmd', '.R', f)
-fr <- gsub('episodes/', '', fr)
-
-
-big <- ''
-
-for(i in fr) {
-    big <- c(big, readLines(i))
-    file.remove(i)
-}
-
-big <- gsub('## ', '', big)
-
-writeLines(big, 'instructors/all_episodes.R')
-
+# f <- c("episodes/introduction.Rmd",
+#        "episodes/care.Rmd",
+#        "episodes/abundance-data.Rmd",
+#        "episodes/traits-data.Rmd",
+#        "episodes/phylo-data.Rmd",
+#        "episodes/popgen-data.Rmd",
+#        "episodes/connecting-data-types.Rmd",
+#        "episodes/finding_data.Rmd",
+#        "episodes/care-data-repos.Rmd")
+#
+# for(i in f) {
+#     knitr::purl(i, documentation = 0)
+# }
+#
+# fr <- gsub('\\.Rmd', '.R', f)
+# fr <- gsub('episodes/', '', fr)
+#
+#
+# big <- ''
+#
+# for(i in fr) {
+#     big <- c(big, readLines(i))
+#     file.remove(i)
+# }
+#
+# big <- gsub('## ', '', big)
+#
+# writeLines(big, 'instructors/all_episodes.R')
+#
